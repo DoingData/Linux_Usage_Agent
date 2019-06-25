51eee4043cb2ec6b018e1981aee6ff11806a8952
Usman <usman.asif.jrvs.ca@gmail.com>
2019-06-25 14:00:43 +0000

    sql query update

--- scripts/init.sql
+++ scripts/init.sql
@@ -23,10 +23,11 @@ ORDER BY cpu_number ASC, total_mem DESC;
 
 --Average used memory in percentage over 5 minutes intervals
 --for each host.( used_memory= total_memory-free_memory)
-SELECT host_usage.host_id,host_info.hostname,host_info.total_mem,
-(host_info.total_mem - host_usage.disk_available) AS used_mem ,
- (((host_info.total_mem - host_usage.disk_available)*100)/ (host_info.total_mem)) AS used_memory_percentage
-FROM host_usage,host_info
-LIMIT 5;
-
+SELECT host_info.id as host_id, host_info.hostname, host_info.total_mem as total_memory,
+ROUND(((AVG(host_info.total_mem - host_usage.memory_free)*100)/host_info.total_mem),0) as used_memory_percentage, 
+(date_trunc('hour', host_usage."timestamp") + date_part('minute',host_usage."timestamp")::int / 5 * interval '5 min') as five_min_interval
+FROM host_info
+INNER JOIN host_usage ON host_usage.host_id = host_info.id 
+GROUP BY five_min_interval, id
+ORDER BY five_min_interval;
 
