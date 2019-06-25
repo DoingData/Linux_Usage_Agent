8a1458ca4bfb5fe5b3e23616081c2019b8f9f763
Usman <usman.asif.jrvs.ca@gmail.com>
2019-06-25 04:48:19 +0000

    some sql additions

--- scripts/init.sql
+++ scripts/init.sql
@@ -14,4 +14,19 @@ CREATE TABLE PUBLIC.host_usage
 	REFERENCES host_info(id)
 );
 
+--Group hosts by CPU number and sort by their
+--memory size in descending oreder(within each cpu group)
+host_agent=# SELECT cpu_number,id,total_mem
+FROM host_info
+ORDER BY cpu_number ASC, total_mem DESC;
+
+
+--Average used memory in percentage over 5 minutes intervals
+--for each host.( used_memory= total_memory-free_memory)
+SELECT host_usage.host_id,host_info.hostname,host_info.total_mem,
+(host_info.total_mem - host_usage.disk_available) AS used_mem ,
+ (((host_info.total_mem - host_usage.disk_available)*100)/ (host_info.total_mem)) AS used_memory_percentage
+FROM host_usage,host_info
+LIMIT 5;
+
 
